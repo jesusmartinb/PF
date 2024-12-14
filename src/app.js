fcd8afbc8573249618f08fe4b35088f3bb1f8df7
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-12-14 11:19:18 +0100

    Se configuran las rutas para /api/carts/ 'POST /', 'GET /:cid' y 'POST /:cid/product/:pid' en el fichero carts.routes.js con persistencia en archivo json por fileSystem

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.use('/api/products', productsRoutes);
 app.use('/api/carts', cartRoutes);
--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -1,6 +1,82 @@
 import { Router } from 'express';
+import fs from 'fs';
 
 const cartRoutes = Router();
+
+const getCarts = async () => {
+    try {
+        const carts = await fs.promises.readFile('src/db/carts.json', 'utf-8');
+        const cartsConverted = JSON.parse(carts);
+        return cartsConverted;
+    } catch (error) {
+        return [];
+    }
+};
+
+const saveCarts = async (carts) => {
+    try {
+        const parsedCarts = JSON.stringify(carts);
+        await fs.promises.writeFile('src/db/carts.json', parsedCarts, 'utf-8');
+        return true;
+    } catch (error) {
+        return false;
+    }
+};
+
+const getSingleCartById = async (cId) => {
+    const carts = await getCarts();
+    const cart = carts.find(cart => cart.id === cId);
+    return cart;
+};
+
+cartRoutes.post('/', async (req, res) => {
+    const products =  [];
+    const cart = {};
+    cart.id = parseInt(Math.floor(Math.random() * 10000).toString() + Date.now().toString());
+    cart.products = products;
+    if(!cart.products || !Array.isArray(cart.products)) {
+        return res.status(400).send({status: 'error', message: 'Carrito vacío'});
+    }
+    const carts = await getCarts();
+    carts.push(cart);
+    const isOk = await saveCarts(carts);
+    if(!isOk) {
+        return res.send({status: 'error', message: 'Error al guardar el carrito'});
+    }
+    res.send({status: 'ok', message: 'Carrito recibido'});
+});
+
+cartRoutes.get('/:cid', async (req, res) => {
+    const cId = +req.params.cid;
+    const cart = await getSingleCartById(cId);
+    if(!cart) {
+        return res.status(404).send({status: 'error', message: 'Carrito no encontrado'});
+    }
+    res.send({cart});
+});
+
+cartRoutes.post('/:cid/product/:pid', async (req, res) => {
+    const cId = +req.params.cid;
+    const pId = +req.params.pid;
+    const carts = await getCarts();
+    const cart = carts.find(cart => cart.id === cId);
+    if(!cart) {
+        return res.status(404).send({status: 'error', message: 'Carrito no encontrado'});
+    }
+    const products = cart.products;
+    const product = products.find(product => product.id === pId);
+    if(!product) {
+        products.push({id: pId, quantity: 1});
+    } else {
+        product.quantity++;
+    }
+    const isOk = await saveCarts(carts);
+    if(!isOk) {
+        return res.send({status: 'error', message: 'Error al guardar el carrito'});
+    }
+    res.send({status: 'ok', message: 'Producto añadido al carrito'});
+});
+
 
 
 export default cartRoutes;

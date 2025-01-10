a74964e51c12f3b76284ba61630b7864e95bb556
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2025-01-10 14:37:36 +0100

    Se instala el motor de plantillas express-handlebars y se actualiza app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 import productsRoutes from './routes/products.routes.js';
 import cartRoutes from './routes/carts.routes.js';
 
@@ -10,6 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.engine('handlebars', handlebars.engine());
+app.set('views', 'src/views');
+app.set('view engine', 'handlebars');
 app.use('/api/products', productsRoutes);
 app.use('/api/carts', cartRoutes);
 
